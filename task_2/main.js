4f0dcb5c99764dfe3a5c4e6389caa49916e8b312
Teslik0 <z12729476@gmail.com>
2024-01-29 11:05:34 +0300

    Создали файл и подключили JS

--- task_2/main.js
+++ task_2/main.js
@@ -1,11 +1,28 @@
-const age = 16;
-
+// Пара 28.01.24
+// задача 2
+/* const age = 16;
 if (age >= 18) {
     console.log('Вы можете работать!');
-} else(age >= 16){
+} else if (age >= 16){
     console.log('Вы можете рабоать с ограничениями!');
-}else if (age >= 14){
+} else if (age >= 14){
     console.log('Вы можете работать с разрешения родителей!');
 }else {
     console.log ('Вы ещё маленький!')
-}
+} */
+ 
+// задача 3
+/*let statusPar = true
+console.log ("Сегодня важные пары?");
+if (statusPar === true) {
+    console.log ("Сколько сегодня пар?")
+    let countPar = 2;
+
+    if (countPar < 4){
+        console.log("Сиди дома")
+    } else {
+        console.log("Беги на учебу")
+    }
+} else {
+    console.log ('Сиди дома')
+}*/
