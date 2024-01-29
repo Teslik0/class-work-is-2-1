aee5b45beefe3c07a2bf649e97720636512b701b
Teslik0 <z12729476@gmail.com>
2024-01-29 10:33:55 +0300

    Написали программу на конструкции if...elseif

--- task_2/main.js
+++ task_2/main.js
@@ -1,1 +1,11 @@
-console.log("Работает");
+const age = 16;
+
+if (age >= 18) {
+    console.log('Вы можете работать!');
+} else(age >= 16){
+    console.log('Вы можете рабоать с ограничениями!');
+}else if (age >= 14){
+    console.log('Вы можете работать с разрешения родителей!');
+}else {
+    console.log ('Вы ещё маленький!')
+}
