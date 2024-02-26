c1cff380464430025a0569a71c0cf60228f34101
Teslik0 <z12729476@gmail.com>
2024-02-26 10:34:28 +0300

    Функции

---
+++ task_6/main.js
@@ -0,0 +1,93 @@
+// балванка
+// // let sum = (a, b, c, d) => { 
+// }
+// sum ()
+
+
+// Пример 1 a + b = ...
+// let sum = (a, b) => {
+//     let a;
+//     let b;
+//     let result = a + b;
+//     console.log(result);
+// }
+// sum(2, 3);
+// sum(5, 7);
+
+
+
+// пример 2 (a * b) + (c * d) = ...
+// let sum = (a, b, c, d) => {
+//     let res1 = a*b;
+//     let res2 = c*d;
+//     let result = res1 + res2;
+//     console.log(result);
+// }
+// sum(2, 3, 4, 5);
+// sum(22,33,44,55);
+// sum(222,333,444,555);
+
+
+// пример 3 (a * b) + (c * d) = ...
+// let sum = (a = 2, b = 3, c = 4, d =) => {
+//     let res1 = a*b;
+//     let res2 = c*d;
+//     let result = res1 + res2;
+//     console.log(result);
+// }
+// sum();
+// sum(12, 15, 16, 17);
+
+
+// пример 4 (a * b) + (c * d) = ...
+// let sum = (a, b, c, d = 5) => {
+//     let res1 = a*b;
+//     let res2 = c*d;
+//     let result = res1 + res2;
+//     console.log(result);
+// }
+// sum();
+// sum(12, 15, 16, 23);
+
+
+// // пример 5 массив или объект
+// let persone = (array) => {
+//     console.log(array[0]);
+// }
+
+// const tom = ["Tom", 42, "tom@example.com"];
+// const bob = ["Bob", 32, "Bob@example.com"];
+// // console.log(tom[0])
+// persone(tom);
+// persone(bob);
+
+
+// пример 6
+// // let persone = (Object) => {
+//     console.log(Object["name"]);
+//     console.log(Object["age"]);
+// }
+
+// let tom = {name: "Tom", age: 42, email: "tom@example.com"};
+// let bob = {name: "Bob", age: 32, email: "Bob@example.com"};
+
+// persone(tom)
+// persone(bob)
+
+
+// пример 7
+// let temp = (e) => {
+//     if (e === undefined) {
+//         console.log ("Укажите тем-ру");
+//     } else {
+//         if (e >= 25) {
+//         console.log(`Сегодня ${e} градусов. Можно надеть футболку`);
+//         }
+//     }
+// }
+
+// let key = prompt("Введите температуру");
+// temp (key);
+
+
+// Пример 8
